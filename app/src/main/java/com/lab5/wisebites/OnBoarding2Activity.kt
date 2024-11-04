37de3536783eda6a16b58b62a63c4795d2838b19
Rio <rioloka86@gmail.com>
2024-11-04 11:40:16 +0700

    feat: add viewPager for OnBoarding Swiping Logic

--- app/src/main/java/com/lab5/wisebites/OnBoarding2Activity.kt
+++ app/src/main/java/com/lab5/wisebites/OnBoarding2Activity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class onBoarding2Activity : AppCompatActivity() {
+class OnBoarding2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
