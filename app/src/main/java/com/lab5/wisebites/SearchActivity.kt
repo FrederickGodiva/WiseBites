05abab0aa332e61279c58eba6fa4f2031e31df20
Rio <rioloka86@gmail.com>
2024-11-10 10:13:00 +0700

    feat: create search activity UI

--- app/src/main/java/com/lab5/wisebites/SearchActivity.kt
+++ app/src/main/java/com/lab5/wisebites/SearchActivity.kt
@@ -4,26 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.lab5.wisebites.databinding.ActivityHomeBinding
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.lab5.wisebites.databinding.ActivitySearchBinding
 
-class HomeActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityHomeBinding
+    private lateinit var binding: ActivitySearchBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnSort.setOnClickListener() {
-            val sortModalBottomSheet = SortModalBottomSheetDialog()
-            sortModalBottomSheet.show(supportFragmentManager, sortModalBottomSheet.tag)
-        }
-
         // Set the default selected item in Navigation Menu
-        binding.bnMenu.selectedItemId = R.id.i_home
+        binding.bnMenu.selectedItemId = R.id.i_search
 
         // Items Selection Handler
         binding.bnMenu.setOnItemSelectedListener { item ->
@@ -35,9 +32,9 @@ class HomeActivity : AppCompatActivity() {
                     true
                 }
                 R.id.i_search -> {
-                     intent = Intent(this, SearchActivity::class.java)
-                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-                     startActivity(intent)
+                    intent = Intent(this, SearchActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
                     true
                 }
                 R.id.i_bookmark -> {
