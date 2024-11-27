fc6b8c8d3701a09913057b0e98755bf5bbe6d8c7
Rio <rioloka86@gmail.com>
2024-11-27 17:06:07 +0700

    feat: add fetchMultipleRecipes in Search Activity

--- app/src/main/java/com/lab5/wisebites/SearchActivity.kt
+++ app/src/main/java/com/lab5/wisebites/SearchActivity.kt
@@ -2,22 +2,32 @@ package com.lab5.wisebites
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.lifecycleScope
 import com.google.firebase.auth.FirebaseAuth
 import com.lab5.wisebites.adapter.RecipeAdapter
+import com.lab5.wisebites.api.APIClient
+import com.lab5.wisebites.api.APIService
 import com.lab5.wisebites.databinding.ActivitySearchBinding
+import com.lab5.wisebites.model.Recipe
 import com.lab5.wisebites.utils.BottomNavigationHandler
 import com.lab5.wisebites.utils.SearchHandler
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SearchActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySearchBinding
+    private lateinit var apiService: APIService
     private lateinit var recipeAdapter: RecipeAdapter
+    private lateinit var recipeList: MutableList<Recipe>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +41,9 @@ class SearchActivity : AppCompatActivity() {
 
         binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        apiService = APIClient.instance.create(APIService::class.java)
+        fetchMultipleRecipes()
 
         // Initialize SearchLogic to set up API service
         SearchHandler.initApiService()
@@ -75,6 +88,33 @@ class SearchActivity : AppCompatActivity() {
         // Use lifecycleScope for proper coroutine handling inside the Activity
         lifecycleScope.launch(Dispatchers.Main) {
             SearchHandler.searchRecipeByName(query, this@SearchActivity, recipeAdapter)
+        }
+    }
+
+    private fun fetchMultipleRecipes() {
+        // Use Coroutine in `lifecycleScope`
+        lifecycleScope.launch {
+            try {
+                binding.cpiSearchRecipes.visibility = View.VISIBLE
+                // Call API 10 times with parallelism
+                recipeList = withContext(Dispatchers.IO) {
+                    (1..8).map {
+                        async { apiService.getRandomRecipe()["meals"]?.firstOrNull() }
+                    }.awaitAll()
+                }.filterNotNull().toMutableList()
+
+                if (recipeList.isNotEmpty()) {
+//                    binding.rvRecipesSearchResult.adapter = RecipeAdapter(
+//                        this@SearchActivity,
+//                        recipeList.toMutableList()
+//                    )
+                    recipeAdapter.updateRecipes(recipeList.toMutableList())
+                }
+            } catch (e: Exception) {
+                Log.e("HomeActivity", "Error: ${e.message}")
+            } finally {
+                binding.cpiSearchRecipes.visibility = View.GONE
+            }
         }
     }
 
